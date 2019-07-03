13588abdb8448011ff7f3592b8761a5786903cb1
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-07-03 14:49:40 +0530

    Adding JumpingOnTheClouds Challenge in Warm-Up

---
+++ InterviewPreparationKit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterviewPreparationKit.Warm_Up;
+
+namespace InterviewPreparationKit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JumpingOnTheClouds.Entry();
+        }
+    }
+}
---
+++ InterviewPreparationKit/Warm-Up/JumpingOnTheClouds.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPreparationKit.Warm_Up
+{
+    class JumpingOnTheClouds
+    {
+        public static void Entry()
+        {
+            int[] c = { 0, 0, 1, 0, 0, 1, 0 };
+            JumpingOnTheClouds obj = new JumpingOnTheClouds();
+            obj.jumpingOnClouds(c);
+        }
+
+        public void jumpingOnClouds(int[] c)
+        {
+            int n = c.Length;
+            int count = -1;
+            for (int i = 0; i < n; i++, count++)
+            {
+                if (i < n - 2 && c[i + 2] == 0) i++;
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
