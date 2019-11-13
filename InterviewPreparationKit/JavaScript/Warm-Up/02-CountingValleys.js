0f35e4967f1dbfbfd4b718461b6ec26b71308bb3
Vibhor Singh <vibhorsinghcs@gmail.com>
2019-11-13 15:12:24 +0530

    Added Challenge 02 to Warm-up

---
+++ InterviewPreparationKit/JavaScript/Warm-Up/02-CountingValleys.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the countingValleys function below.
+function countingValleys(n, s) {
+    let count=0;
+    let res=0;
+    for(let i=0;i<n;i++) {
+        if(s[i] == 'D') { count--; }
+        else if (s[i] == 'U') { count++;
+        if(count == 0) {
+            res++;
+        }}
+    }
+    return res;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const s = readLine();
+
+    let result = countingValleys(n, s);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
